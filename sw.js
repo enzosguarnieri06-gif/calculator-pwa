bd9d1eaf882aa3e62b031033f7a9c03ad667a06f
enzosguarnieri06-gif <enzo.sguarnieri06@gmail.com>
2025-09-08 08:15:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
-  console.log "service-worker instalado" 
+  console.log ("service-worker instalado") 
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(FILES_TO_CACHE);
@@ -41,3 +41,4 @@ self.addEventListener('fetch', (event) => {
   );
 
 });
+
